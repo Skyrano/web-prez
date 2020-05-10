4fb2a9a543bac377f7d064e8f9aeb4b4c56cbce4
Kieran <unconfigured@null.spigotmc.org>
2020-05-10 14:23:56 +0200

    creation des components

--- GRP10-projet-web/src/app/app.module.ts
+++ GRP10-projet-web/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ErreurComponent } from './erreur/erreur.component';
+import { MainComponent } from './main/main.component';
+import { FootComponent } from './foot/foot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ErreurComponent,
+    MainComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule,
