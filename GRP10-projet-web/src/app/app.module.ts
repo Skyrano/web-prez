3903e19f4b75916fb069f6d70906045f3f828a2b
Skyrano <alistair-rameau@hotmail.fr>
2020-05-11 11:30:43 +0200

    Ajout du component map

--- GRP10-projet-web/src/app/app.module.ts
+++ GRP10-projet-web/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FourOfourComponent } from './four-ofour/four-ofour.component';
 import { MentionLegalesComponent } from './mention-legales/mention-legales.component';
 import { HttpClientModule } from "@angular/common/http";
 import { HttpClientService } from './services/httpclient.service';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { HttpClientService } from './services/httpclient.service';
     ContactComponent,
     AideComponent,
     FourOfourComponent,
-    MentionLegalesComponent
+    MentionLegalesComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
---
+++ GRP10-projet-web/src/app/map/map.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+  }
+
+}
