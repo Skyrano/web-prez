521a49866f38e90489f66105c9833bbb960b8500
Kieran <unconfigured@null.spigotmc.org>
2020-05-21 11:37:34 +0200

    mise en forme des pourcentages, candidats dans l'ordre

--- GRP10-projet-web/src/app/candidats/candidats.component.ts
+++ GRP10-projet-web/src/app/candidats/candidats.component.ts
@@ -1,17 +1,30 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-candidats',
   templateUrl: './candidats.component.html',
   styleUrls: ['./candidats.component.scss']
 })
-export class CandidatsComponent implements OnInit {
+export class CandidatsComponent implements OnInit, OnChanges {
 
   @Input() candidats;
+  sortedCandidats = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.sortedCandidats = [];
+    for (let candidat of this.candidats) {
+      this.sortedCandidats.push(candidat);
+    }
+    this.sortedCandidats.sort(function(a,b)
+      {
+        return b.pourcentage - a.pourcentage;
+      });
+    console.log(this.sortedCandidats);
+}
 
 }
